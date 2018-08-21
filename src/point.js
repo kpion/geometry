d393ac600dac8a42a4fc6701feb27eea79acfb8d
konradpapala <konradp@wp.pl>
2018-08-22 01:05:11 +0200

    "42.0" - i.e. number as string also accepted in constructor

--- src/point.js
+++ src/point.js
@@ -1,8 +1,6 @@
 
 /*
-Simple Point class by Konrad Papała - https://github.com/kpion 
-
-License: MIT
+Simple Point class by Konrad Papała - https://github.com/kpion/geometry 
 
 Examples:
 
@@ -10,7 +8,6 @@ var point1 = new Point(100,42);
 var point2 = new Point([10,10]);//different input, also accepted is another Point object
 
 console.log(point1);
-
 point1.add(1,1);//point1 is now: 101,43
 point1.add(point2);//point1 is now: 111,53
 console.log(point1.x, point1.y);
@@ -35,17 +32,18 @@ class Point{
     point.set(42);//x and y set to '42'. Useful e.g. with the this.multiply method. 
     */
     set(x = null,y = null){
-        if(typeof x === 'number'){
-            this.x = x;
+        //12.3 or "12.3"
+        if(typeof x === 'number' || typeof x === 'string'){
+            this.x = parseFloat(x);
             if(y !== null){
-                this.y = y;
+                this.y = parseFloat(y);
             }else{
                 this.y = x;//so, based on one value.
             }
         }else if (Array.isArray(x)) {
             let point = x; //readability
-            this.x = point[0];
-            this.y = point[1];
+            this.x = parseFloat(point[0]);
+            this.y = parseFloat(point[1]);
         }else if(typeof x === 'object'){//we (Point) or any other object having x,y properties
             let point = x; //readability
             this.x = point.x;
@@ -89,6 +87,18 @@ class Point{
     scale(x,y){
         return this.multiply(x,y);
     }
+
+
+    /**
+     * see this.set for possible x,y values.
+     * examples: point.divide(2,2);point.divide(2);
+    */
+    divide(x, y){
+        let point = new Point(x,y); //this will handle the 'x' being numbers, array, a simple object, or another Point 
+        this.x /= point.x;
+        this.y /= point.y;
+        return this;  
+    }    
 
     //see this.set for possible x,y values
     equals(x,y){
